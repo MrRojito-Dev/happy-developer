5185936a89344c37e8331688eefd47cad84b8cea
Michael <michael.m.liendo.r@gmail.com>
2021-05-22 00:22:56 -0400

    xd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let happyDeveloperMotivatorMessages;
-let happyDeveloperOtakusMessages;
+let happyDeveloperOtakuMessages;
 
 if (Intl.DateTimeFormat().resolvedOptions().locale.includes("es")) {
     happyDeveloperMotivatorMessages = [
@@ -31,7 +31,7 @@ if (Intl.DateTimeFormat().resolvedOptions().locale.includes("es")) {
         'There are two ways to write error-free programs; only the third works'
     ];
 
-    happyDeveloperOtakusMessages = [
+    happyDeveloperOtakuMessages = [
         'You are an incredible being, you gave your best and that is why I admire you. You went through various transformations, you were so powerful that we all hated you.',
         'Do not live with falsehoods or fears, because you will end up hating yourself.',
         'I do not care if the gods exist or not, I am the master of my life; I pass from religious themes, but I will never look down on someone who believes in them.',
@@ -44,89 +44,10 @@ let happyDeveloperMotivatorMessage = happyDeveloperMotivatorMessages[Math.floor(
 let happyDeveloperOtakuMessage = happyDeveloperOtakuMessages[Math.floor(Math.random() * happyDeveloperOtakuMessages.length)]
 
 function motivator() {
-    console.log(`\x1b[34m[HAPPY DEVELOPER] \x1b[37m${happyDeveloperMotivatorMessage}\x1b[0m`);
+    console.log(`\x1b[34m[HAPPY DEVELOPER] \x1b[37m${happyDeveloperMotivatorMessage}\x1b[0m`)
 }
 motivator.otakus = function () {
     console.log(`\x1b[34m[HAPPY DEVELOPER] \x1b[37m${happyDeveloperOtakuMessage}\x1b[0m`)
 }
 module.exports = motivator;
 
-
-
-/*
-const colores = {
-    hexLiteral: 0xffffff, //number
-    hexString: 'ffffff',
-    number: 16777215,
-    RGBArray: [255, 255, 255],
-    preDefinidos: [
-        'DEFAULT',
-        'WHITE',
-        'AQUA',
-        'GREEN',
-        'BLUE',
-        'PURPLE',
-        'LUMINOUS_VIVID_PINK',
-        'GOLD',
-        'ORANGE',
-        'RED',
-        'GREY',
-        'DARKER_GREY',
-        'NAVY',
-        'DARK_AQUA',
-        'DARK_GREEN',
-        'DARK_BLUE',
-        'DARK_PURPLE',
-        'DARK_VIVID_PINK',
-        'DARK_GOLD',
-        'DARK_ORANGE',
-        'DARK_RED',
-        'DARK_GREY',
-        'LIGHT_GREY',
-        'DARK_NAVY',
-        'RANDOM'
-    ]
-}
-
-//Los colores son solo numeros que van del 16777215 al 0, puedes verlos como hexadecimal FFFFFF o rgb 255 255 255
-
-//ff en decimal es 255 (si se dan cuenta, son todos lo mismo)
-
-
-Documentacion: https://discord.js.org/#/docs/main/stable/typedef/ColorResolvable
-
-https://www.mathsisfun.com/hexadecimal-decimal-colors.html 
-De ahi pueden sacar todos los colores, medio fea la pagina, pero pueden sacar en todos los formatos
-
-
-//colores para la consola:
-
-Reset = "\x1b[0m"
-Bright = "\x1b[1m"
-Dim = "\x1b[2m"
-Underscore = "\x1b[4m"
-Blink = "\x1b[5m"
-Reverse = "\x1b[7m"
-Hidden = "\x1b[8m"
-
-FgBlack = "\x1b[30m"
-FgRed = "\x1b[31m"
-FgGreen = "\x1b[32m"
-FgYellow = "\x1b[33m"
-FgBlue = "\x1b[34m"
-FgMagenta = "\x1b[35m"
-FgCyan = "\x1b[36m"
-FgWhite = "\x1b[37m"
-
-BgBlack = "\x1b[40m"
-BgRed = "\x1b[41m"
-BgGreen = "\x1b[42m"
-BgYellow = "\x1b[43m"
-BgBlue = "\x1b[44m"
-BgMagenta = "\x1b[45m"
-BgCyan = "\x1b[46m"
-BgWhite = "\x1b[47m"
-
-//Ejemplo de uso:
-console.log('\x1b[34mEsto estara de color azul\x1b[0m y esto estara normal')
- */
